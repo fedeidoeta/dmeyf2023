c7df6f48533dc688097fe4ff2618448d20a9a84c
fedeidoeta <federicoidoeta@gmail.com>
2023-09-10 21:20:04 -0300

    fecha	minsplit	minbucket	maxdepth	corte	cp	ganancia	iteracion	Oversampling	Seed kaggle_03	820	376	10	9500	-1	71,4	k3	1	270001 kaggle_04	820	376	10	9500	-1	71,4	k3	1	270029

--- src/rpart/fi_kaggle_1.r
+++ src/rpart/fi_kaggle_1.r
@@ -17,8 +17,34 @@ dir.create("./exp/", showWarnings = FALSE)
 dir.create("./exp/KG0001/", showWarnings = FALSE)
 setwd("./exp/KG0001")
 
-# uso esta semilla para los canaritos
-set.seed(270001)
+# seteo semilla
+set.seed(270029) #270001, 270029, 270031, 270037, 270059, 270071
+
+"####################### Test canarios ###################
+# agrego canaritos randomizados
+dataset2 <- copy(dataset)
+# quito algunas variables de dataset2
+dataset2[ , numero_de_cliente := NULL ]
+dataset2[ , clase_ternaria := NULL ]
+dataset2[ , foto_mes := NULL ]
+
+# agrego azar
+dataset2[ , azar := runif( nrow(dataset2) ) ]
+# randomizo, manteniendo las relaciones entre las variables
+setorder( dataset2, azar )
+dataset2[ , azar := NULL ]  # borra azar
+
+columnas <- copy(colnames(dataset2))
+
+# creo efectivamente los canaritos
+#  1/5  de las variables del dataset
+for( i in sample( 1:ncol(dataset2) , round( ncol(dataset)/5 ) )  )
+{
+  dataset[, paste0("canarito", i) :=  dataset2[ , get(columnas[i]) ]  ]
+}
+##################################################
+"
+
 # defino la clase_binaria2
 dataset[ , clase_binaria := ifelse( clase_ternaria=="CONTINUA", "NEG", "POS" ) ]
 
@@ -29,20 +55,19 @@ dapply[ , clase_ternaria := NA ]
 # Seteo pesos para oversampling
 pesos <- copy( dtrain[, ifelse( clase_ternaria=="CONTINUA",   1.0, 100.0  ) ])
 
-#Modelo con hiperparametros optimizados
+#Modelo con optimizacion Bayesiana de hiperparametros de rpart (hiperparametros finales)
 modelo <- rpart(
     formula = "clase_binaria ~ . - clase_ternaria",
     data = dtrain,
     model = TRUE,
     xval = 0,
     cp = -1,
-    minsplit = 820, # dejo que crezca y corte todo lo que quiera
+    minsplit = 820,
     minbucket = 376,
     maxdepth = 10,
     weight = pesos
 )
-
-
+# Prediccion al dataset 202105
 prediccion <- predict(modelo, dapply, type = "prob")
 
 # esta es la probabilidad de baja
@@ -52,14 +77,16 @@ tablita <- copy( dapply[, list(numero_de_cliente) ] )
 tablita[ , prob := prob_baja ]
 setorder( tablita, -prob )
 
-PARAM$corte <- 9612
+PARAM$corte <- 9500
 
 # grabo el submit a Kaggle
 tablita[ , Predicted := 0L ]
 tablita[ 1:PARAM$corte, Predicted := 1L ]
 
-fwrite(tablita[ , list(numero_de_cliente, Predicted)], paste0("kaggle_02.csv"), sep = ",")
+#Se guarda archivo para enviar a kaggle
+fwrite(tablita[ , list(numero_de_cliente, Predicted)], paste0("kaggle_04.csv"), sep = ",")
 
-pdf(file = "./modelo_kaggle_2.pdf", width=28, height=4)
+#Se guarda imagen del arbol resultante 
+pdf(file = "./modelo_kaggle_4.pdf", width=28, height=4)
 prp(modelo, extra=101, digits=5, branch=1, type=4, varlen=0, faclen=0)
 dev.off()
