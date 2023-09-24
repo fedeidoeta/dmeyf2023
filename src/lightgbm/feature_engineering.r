f49ac38b238f02afeb8f143f53389f2714035b0d
fedeidoeta <federicoidoeta@gmail.com>
2023-09-24 17:08:55 -0300

    Check Lag

--- src/lightgbm/feature_engineering.r
+++ src/lightgbm/feature_engineering.r
@@ -20,7 +20,7 @@ PARAM$input$future <- c(202107) # meses donde se aplica el modelo
 # cargo el dataset donde voy a entrenar
 dataset <- fread(PARAM$input$dataset, stringsAsFactors = TRUE)
 
-head(dataset[,8:10], 1)
+dataset[1,8:10]
 
 columns <- c("ctrx_quarter", 
 "mcaja_ahorro",
@@ -67,7 +67,7 @@ all_columns <- setdiff(
   colnames(dataset),
   c("numero_de_cliente", "foto_mes", "clase_ternaria")
 )
-unique(dataset$foto_mes)
+
 setorder(dataset, numero_de_cliente, foto_mes)
 
 periods <- seq(1, 3) # Seleccionar cantidad de periodos 
@@ -76,5 +76,45 @@ for (i in periods){
     lagcolumns <- paste("lag", all_columns,i, sep=".")
     dataset[, (lagcolumns):= shift(.SD, type = "lag", fill = NA, n=i), .SDcols = all_columns,  by =numero_de_cliente]
 }
-# Check de los primeros 3 meses
+# Check de los primeros 3 meses y 100 registros
 head(dataset[, c("numero_de_cliente", "ctrx_quarter", "lag.ctrx_quarter.1", "lag.mautoservicio.2")], 100)
+
+
+##################################################################################################
+# Coloca NA a todos los registros en 0
+
+# Antes de los NA
+
+all_columns <- setdiff(
+  colnames(dataset),
+  c("numero_de_cliente", "foto_mes", "clase_ternaria")
+)
+
+dataset[1:3, ..all_columns]
+
+dataset[, (all_columns) := lapply(.SD, function(x) ifelse(x == 0, NA, x)), .SDcols = all_columns]
+
+#Despues de NA
+head(dataset,5)
+
+###################################################################################################
+# Ranking en order descendente de cada feature respecto al mes de cada cliente, 
+# frente a empate igualo posicion
+
+all_columns <- setdiff(
+  colnames(dataset),
+  c("numero_de_cliente", "foto_mes", "clase_ternaria")
+)
+
+for (col in all_columns){
+    rankcolumns <- paste("rank", col, sep=".")
+    dataset[, (rankcolumns):= frank(-.SD[[col]], ties.method= "dense"), by = foto_mes]
+}
+
+## Check de creacion de rank features
+
+dataset[1:100, c("numero_de_cliente","foto_mes", "ctrx_quarter", "rank.ctrx_quarter")]
+setorder(dataset, foto_mes, rank.mcuentas_saldo)
+dataset[rank.mcuentas_saldo<=2 & foto_mes==202006, c("numero_de_cliente","foto_mes", "mcuentas_saldo", "rank.mcuentas_saldo")]
+
+all_columns
