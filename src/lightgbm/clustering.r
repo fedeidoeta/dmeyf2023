9e913b0db339e4397bfa3c286c5b835ebbeb58c5
fedeidoeta <federicoidoeta@gmail.com>
2023-10-10 08:17:43 -0300

    Clustering con graficos

--- src/lightgbm/clustering.r
+++ src/lightgbm/clustering.r
@@ -80,3 +80,144 @@ ggplot() + geom_point(aes(x = 1:20, y = wcss), color = 'blue') +
   ylab('WCSS')
 
 dev.off()
+
+
+###########################################################
+# Analisis de los resultados
+
+
+# limpio la memoria
+rm(list = ls()) # remove all objects
+gc() # garbage collection
+
+require("data.table")
+require("rlist")
+
+PARAM <- list()
+
+PARAM$input$dataset <- "./competencia_02.csv.gz"
+PARAM$input$cluster <- "./CLU_2.csv"
+PARAM$experimento <- "CLU_hist"
+
+setwd("./datasets/")
+
+dataset <- fread(PARAM$input$dataset, stringsAsFactors = TRUE)
+clu <- fread(PARAM$input$cluster, stringsAsFactors = TRUE)
+
+# creo la carpeta donde va el experimento
+dir.create(paste0("./exp/", PARAM$experimento, "/"), showWarnings = FALSE)
+# Establezco el Working Directory DEL EXPERIMENTO
+setwd(paste0("../exp/", PARAM$experimento, "/"))
+
+dataset[1,8:10]
+
+data_clust_hist <- dataset[clu, on = "numero_de_cliente"]
+
+#Selecciono periodos para analizar la media
+
+periodos <- c(202101, 202102, 202103, 202104, 202105)
+
+data_clust_hist_select <- data_clust_hist[foto_mes %in% periodos]
+
+columnas_numericas <- colnames(data_clust_hist_select)[sapply(data_clust_hist_select, is.numeric)]
+
+resultados <- data_clust_hist_select[, lapply(.SD, mean), by = rf.clusters, .SDcols = columnas_numericas]
+
+fwrite(resultados,file = paste0(PARAM$experimento, ".csv"),sep = ",")
+
+head(data_clust_hist,2)
+
+#######################################################################
+# Graficos de todo la historia del dataset
+
+#library(ggplot2)
+
+all_columns <- setdiff(
+  colnames(data_clust_hist),
+  c("numero_de_cliente", "foto_mes", "clase_ternaria","i.foto_mes","rf.clusters")
+)
+
+columns <- c("ctrx_quarter", 
+"mcaja_ahorro",
+"mcuentas_saldo")
+
+#campo <- "ctrx_quarter"
+pdf( paste0("mean", ".pdf"))
+
+for (campo in all_columns) {
+
+tbl <- data_clust_hist[
+    ,
+    list("mean" = sum(get(campo), na.rm = TRUE) / .N),
+    by= .(rf.clusters,foto_mes)
+  ]
+
+# Excluye los ultimos dos periodos
+tbl <- subset(tbl, !(foto_mes %in% c(202107, 202106)))
+
+# Crear un gráfico de líneas múltiples
+p <- ggplot(data = tbl, aes(x = as.factor(foto_mes), y = mean, group = rf.clusters, color = as.factor(rf.clusters))) +
+  geom_line() +
+  labs(
+    title = paste0("Media de ", campo, " por Cluster"),
+    x = "Periodo",
+    y = "Media"
+  ) +
+  theme_minimal() +
+  scale_x_discrete(labels = tbl$foto_mes) +
+  theme(text = element_text(size = 7),
+
+        axis.text.x = element_text(size = 5),
+        legend.text = element_text(size = 5))
+
+print(p)
+}
+
+dev.off()
+
+############################################################################
+######################## NO SIRVE ##########################################
+# Graficos de un periodo seleccionado
+
+#library(ggplot2)
+
+all_columns <- setdiff(
+  colnames(data_clust_hist),
+  c("numero_de_cliente", "foto_mes", "clase_ternaria","i.foto_mes","rf.clusters")
+)
+
+# Selecciono periodos a graficar
+periodos <- c(202101, 202102, 202103, 202104, 202105)
+
+pdf( paste0("mean_periodos", ".pdf"))
+
+for (campo in all_columns) {
+
+tbl <- data_clust_hist[
+    ,
+    list("mean" = sum(get(campo), na.rm = TRUE) / .N),
+    by= .(rf.clusters,foto_mes)
+  ]
+
+#Filtra periodos seleccionados
+tbl_2 <- tbl[foto_mes %in% periodos]
+
+# Crear un gráfico de líneas múltiples
+p <- ggplot(data = tbl_2, aes(x = as.factor(foto_mes), y = mean, group = rf.clusters, color = as.factor(rf.clusters))) +
+  geom_line() +
+  labs(
+    title = paste0("Media de ", campo, " por Cluster"),
+    x = "Periodo",
+    y = "Media"
+  ) +
+  theme_minimal() +
+  scale_x_discrete(labels = tbl_2$foto_mes) +
+  theme(text = element_text(size = 7),
+        axis.text.x = element_text(size = 5),
+        legend.text = element_text(size = 5))
+
+print(p)
+}
+
+dev.off()
+
