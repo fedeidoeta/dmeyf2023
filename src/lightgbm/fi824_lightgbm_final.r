badb1c2ca4e93abeaf95059b76d0a0ee38b424eb
fedeidoeta <federicoidoeta@gmail.com>
2023-10-15 20:51:19 -0300

    #fi824_final: # - Validacion del modelo elegido # - Periodo de train: c(201907, 201908, 201909, 201910, 201911, #                          201912, 202011, 202012, 202101, 202102, #                          202103, 202104, 202105) # + Utilizo los mejores hiperparametros de fi823_under_3 # = Agrego lag de 6 meses de cada feature # + Agrego delta lag de los primeros seis periodos # = Reemplazo 0 por NA en meses y features selectos # = Rankeo a cada cliente respecto de cada mes en cada feature dejando fijo el 0

--- src/lightgbm/fi824_lightgbm_final.r
+++ src/lightgbm/fi824_lightgbm_final.r
@@ -2,7 +2,8 @@
 #   8 vCPU
 #  256 GB memoria RAM
 
-#fi824_5:
+#fi824_final:
+# - Validacion del modelo elegido
 # - Periodo de train: c(201907, 201908, 201909, 201910, 201911, 
 #                          201912, 202011, 202012, 202101, 202102, 
 #                          202103, 202104, 202105)
@@ -24,7 +25,7 @@ require("lightgbm")
 # defino los parametros de la corrida, en una lista, la variable global  PARAM
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
-PARAM$experimento <- "KA8240_5"
+PARAM$experimento <- "KA8240_final"
 
 PARAM$input$dataset <- "./datasets/competencia_02.csv.gz"
 
@@ -35,7 +36,7 @@ PARAM$input$training <- c(201907, 201908, 201909, 201910, 201911,
 
 PARAM$input$future <- c(202107) # meses donde se aplica el modelo
 
-PARAM$finalmodel$semilla <- 270029
+
 
 # hiperparametros intencionalmente NO optimos
 PARAM$finalmodel$optim$num_iterations <- 1027 # 1343 -> 1865 -> 1539 -> 1027
@@ -46,37 +47,6 @@ PARAM$finalmodel$optim$num_leaves <- 501 #1015 -> 434 -> 795 -> 501
 
 
 
-# Hiperparametros FIJOS de  lightgbm
-PARAM$finalmodel$lgb_basicos <- list(
-  boosting = "gbdt", # puede ir  dart  , ni pruebe random_forest
-  objective = "binary",
-  metric = "custom",
-  first_metric_only = TRUE,
-  boost_from_average = TRUE,
-  feature_pre_filter = FALSE,
-  force_row_wise = TRUE, # para reducir warnings
-  verbosity = -100,
-  max_depth = -1L, # -1 significa no limitar,  por ahora lo dejo fijo
-  min_gain_to_split = 0.0, # min_gain_to_split >= 0.0
-  min_sum_hessian_in_leaf = 0.001, #  min_sum_hessian_in_leaf >= 0.0
-  lambda_l1 = 0.0, # lambda_l1 >= 0.0
-  lambda_l2 = 0.0, # lambda_l2 >= 0.0
-  max_bin = 31L, # lo debo dejar fijo, no participa de la BO
-
-  bagging_fraction = 1.0, # 0.0 < bagging_fraction <= 1.0
-  pos_bagging_fraction = 1.0, # 0.0 < pos_bagging_fraction <= 1.0
-  neg_bagging_fraction = 1.0, # 0.0 < neg_bagging_fraction <= 1.0
-  is_unbalance = FALSE, #
-  scale_pos_weight = 1.0, # scale_pos_weight > 0.0
-
-  drop_rate = 0.1, # 0.0 < neg_bagging_fraction <= 1.0
-  max_drop = 50, # <=0 means no limit
-  skip_drop = 0.5, # 0.0 <= skip_drop <= 1.0
-
-  extra_trees = TRUE, # Magic Sauce
-
-  seed = PARAM$finalmodel$semilla
-)
 
 
 #------------------------------------------------------------------------------
@@ -232,6 +202,41 @@ dtrain <- lgb.Dataset(
 )
 
 
+PARAM$finalmodel$semilla <- 270029
+
+
+# Hiperparametros FIJOS de  lightgbm
+PARAM$finalmodel$lgb_basicos <- list(
+  boosting = "gbdt", # puede ir  dart  , ni pruebe random_forest
+  objective = "binary",
+  metric = "custom",
+  first_metric_only = TRUE,
+  boost_from_average = TRUE,
+  feature_pre_filter = FALSE,
+  force_row_wise = TRUE, # para reducir warnings
+  verbosity = -100,
+  max_depth = -1L, # -1 significa no limitar,  por ahora lo dejo fijo
+  min_gain_to_split = 0.0, # min_gain_to_split >= 0.0
+  min_sum_hessian_in_leaf = 0.001, #  min_sum_hessian_in_leaf >= 0.0
+  lambda_l1 = 0.0, # lambda_l1 >= 0.0
+  lambda_l2 = 0.0, # lambda_l2 >= 0.0
+  max_bin = 31L, # lo debo dejar fijo, no participa de la BO
+
+  bagging_fraction = 1.0, # 0.0 < bagging_fraction <= 1.0
+  pos_bagging_fraction = 1.0, # 0.0 < pos_bagging_fraction <= 1.0
+  neg_bagging_fraction = 1.0, # 0.0 < neg_bagging_fraction <= 1.0
+  is_unbalance = FALSE, #
+  scale_pos_weight = 1.0, # scale_pos_weight > 0.0
+
+  drop_rate = 0.1, # 0.0 < neg_bagging_fraction <= 1.0
+  max_drop = 50, # <=0 means no limit
+  skip_drop = 0.5, # 0.0 <= skip_drop <= 1.0
+
+  extra_trees = TRUE, # Magic Sauce
+
+  seed = PARAM$finalmodel$semilla
+)
+
 # genero el modelo
 param_completo <- c(PARAM$finalmodel$lgb_basicos,
   PARAM$finalmodel$optim)
@@ -283,7 +288,7 @@ setorder(tb_entrega, -prob)
 # suba TODOS los archivos a Kaggle
 # espera a la siguiente clase sincronica en donde el tema sera explicado
 
-cortes <- seq(9000, 12000, by = 200)
+cortes <- seq(8000, 10500, by = 500)
 for (envios in cortes) {
   tb_entrega[, Predicted := 0L]
   tb_entrega[1:envios, Predicted := 1L]
