a72786248cb76e08a1b6f416681d2f5896f3a130
fedeidoeta <federicoidoeta@gmail.com>
2023-11-18 08:28:41 -0300

    Missing values

--- src/CatastropheAnalysis/missing_values.r
+++ src/CatastropheAnalysis/missing_values.r
@@ -5,15 +5,19 @@
 rm(list = ls()) # remove all objects
 gc() # garbage collection
 
+#install.packages("mice")
 require("data.table")
 require("rlist")
+#library(VIM)
+require("VIM")
+require("mice")
 
 
 PARAM <- list()
 PARAM$input$dataset <- "./datasets/competencia_02.csv.gz"
 
 #setwd("~/buckets/b1")
-#setwd("C:/Users/feder/Documents/Maestria_en_Ciencia_de_datos/4_DM_en_Economia_y_Finanzas")
+setwd("C:/Users/feder/Documents/Maestria_en_Ciencia_de_datos/4_DM_en_Economia_y_Finanzas")
 
 # cargo el dataset donde voy a entrenar
 dataset <- fread(PARAM$input$dataset, stringsAsFactors = TRUE)
@@ -64,7 +68,7 @@ zero_ratio <- list(
     c("mtransferencias_recibidas","ctransferencias_recibidas")),
   list(mes = 201904, campo = 
     c("ctarjeta_visa_debitos_automaticos","mttarjeta_visa_debitos_automaticos",
-    "mtransferencias_recibidas","ctransferencias_recibidas","ctarjeta_visa_debitos_automaticos")),
+    "mtransferencias_recibidas","ctransferencias_recibidas")),
   list(mes = 201905, campo = 
     c("mrentabilidad", "mrentabilidad_annual", "mcomisiones","mactivos_margen","mpasivos_margen",
     "ccomisiones_otras","mcomisiones_otras","mtransferencias_recibidas","ctransferencias_recibidas")),
@@ -94,6 +98,9 @@ for (par in zero_ratio) {
 ## Check 
 head(dataset[foto_mes<=201910 & numero_de_cliente == 29183981, c("numero_de_cliente","foto_mes","mcomisiones_otras")],10)
 
+############################
+
+#Imputa por la media
 
 imputar_media <- function(campo, meses){
 
@@ -109,8 +116,9 @@ imputar_media <- function(campo, meses){
 
 dataset <- imputar_media("mcomisiones_otras", c(201905,201910))
 
+############################
 
-
+#Imputa por la mediana
 
 imputar_mediana <- function(campo, meses){
 
@@ -125,3 +133,87 @@ imputar_mediana <- function(campo, meses){
 }
 
 dataset <- imputar_mediana("mcomisiones_otras", c(201905,201910))
+
+############################
+
+#Imputa por el valor anterior
+
+imputar_anterior <- function(campo, meses) {
+  dataset <- dataset[order(numero_de_cliente, foto_mes)]
+  
+  for (mes in meses) {
+    variable_lagueada <- paste0(campo, "_anterior")
+    dataset[, (variable_lagueada) := shift(get(campo), type = "lag", fill = NA), by = numero_de_cliente]
+    dataset[foto_mes == mes, (campo) := ifelse(is.na(get(campo)), get(variable_lagueada), get(campo))]
+    dataset[, (variable_lagueada) := NULL]
+  }
+  
+  return(dataset)
+}
+
+dataset <- imputar_anterior("mcomisiones_otras", c(201905,201910))
+
+############################
+
+#Imputa por KNN
+
+# Coloco en NA las variables de los meses de preferencia
+zero_ratio <- list(
+  list(mes = 201901, campo = 
+    c("mtransferencias_recibidas","ctransferencias_recibidas")),
+  list(mes = 201902, campo = 
+    c("mtransferencias_recibidas","ctransferencias_recibidas")),
+  list(mes = 201903, campo = 
+    c("mtransferencias_recibidas","ctransferencias_recibidas")),
+  list(mes = 201904, campo = 
+    c("ctarjeta_visa_debitos_automaticos","mttarjeta_visa_debitos_automaticos",
+    "mtransferencias_recibidas","ctransferencias_recibidas")),
+  list(mes = 201905, campo = 
+    c("mrentabilidad", "mrentabilidad_annual", "mcomisiones","mactivos_margen","mpasivos_margen",
+    "ccomisiones_otras","mcomisiones_otras","mtransferencias_recibidas","ctransferencias_recibidas")),
+  list(mes = 201910, campo = 
+    c("mrentabilidad", "mrentabilidad_annual","mcomisiones","mactivos_margen","mpasivos_margen",
+    "ccomisiones_otras","mcomisiones_otras","chomebanking_transacciones")),
+  list(mes = 202006, campo = 
+    c("active_quarter", "internet", "mrentabilidad", "mrentabilidad_annual", 
+      "mcomisiones", "mactivos_margen", "mpasivos_margen", "mcuentas_saldo", 
+      "ctarjeta_debito_transacciones","mautoservicio", "ctarjeta_visa_transacciones", 
+      "mtarjeta_visa_consumo","ctarjeta_master_transacciones", "mtarjeta_master_consumo",
+      "ccomisiones_otras", "mcomisiones_otras","cextraccion_autoservicio","mextraccion_autoservicio",
+      "ccheques_depositados","mcheques_depositados","ccheques_emitidos","mcheques_emitidos",
+      "ccheques_depositados_rechazados","mcheques_depositados_rechazados","ccheques_emitidos_rechazados",
+      "mcheques_emitidos_rechazados","tcallcenter","ccallcenter_transacciones","thomebanking",
+      "chomebanking_transacciones","ccajas_transacciones","ccajas_consultas","ccajas_depositos",
+      "ccajas_extracciones","ccajas_otras","catm_trx","matm","catm_trx_other","matm_other",
+      "tmobile_app","cmobile_app_trx"))
+)
+
+for (par in zero_ratio) {
+  mes <- par$mes
+  feature <- par$campo
+  dataset[foto_mes == mes, (feature) := lapply(.SD, function(x) ifelse(x == 0, NA, x)), .SDcols = feature]
+}
+
+tbl <- dataset[ foto_mes <= mes, ]
+tbl.imputada <-  kNN(tbl, k = 6)
+
+
+
+
+imputacion <- mice(airquality[c("Ozone","Solar.R")], m = 3, maxit = 10, print = FALSE, seed = 270001)
+
+####Pensar KNN
+imputar_KNN <- function(campo, meses) {
+  dataset <- dataset[order(numero_de_cliente, foto_mes)]
+  
+  for (mes in meses) {
+        tbl <- dataset[ foto_mes < mes, get(campo), by = numero_de_cliente]
+        tbl <- tbl[, mice(tbl, seed=270001), by = numero_de_cliente]
+        dataset <- dataset[tbl, on= .(numero_de_cliente)]
+        dataset[foto_mes == mes, paste0(campo) := ifelse(get(campo) == 0, V1, get(campo))]
+        dataset [, V1 := NULL]
+    }
+    return(dataset)
+}
+
+dataset <- imputar_anterior("mcomisiones_otras", c(201905,201910))
